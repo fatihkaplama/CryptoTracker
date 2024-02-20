2330e86763cadb8c82e6b8fba7a8a8eaf04dfc09
Fatih Kaplama <63496655+fatihkaplama@users.noreply.github.com>
2024-02-20 22:15:39 +0300

    Add user's porfolio feature

---
+++ CryptoTracker/Core/Components/CoinImage/CoinLogoView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinLogoView.swift
+//  CryptoTracker
+//
+//  Created by Fatih Kaplama on 12.02.2024.
+//
+
+import SwiftUI
+
+struct CoinLogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinLogoView()
+}
---
+++ CryptoTracker/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioView.swift
+//  CryptoTracker
+//
+//  Created by Fatih Kaplama on 12.02.2024.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
